5d297fb8dcdbafcfb8fc3ff1ce75759174b77b73
ArchelsRamadhany <102032677+ArchelsRamadhany@users.noreply.github.com>
2022-03-21 19:58:33 +0800

    Create MovieRecyclerViewAdapter.java

---
+++ modules/MovieRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.project.recyclerviewdatamovie;
+
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.MovieViewHolder> {
+    ArrayList<Movies> arrayListMovies;
+
+    public MovieRecyclerViewAdapter(ArrayList<Movies> arrayListMovies) {
+        this.arrayListMovies = arrayListMovies;
+    }
+
+    @NonNull
+    @Override
+    public MovieRecyclerViewAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieRecyclerViewAdapter.MovieViewHolder holder, int position) {
+        final Movies movies = arrayListMovies.get(position);
+
+        holder.textViewTitle.setText(movies.getTitle());
+        holder.textViewRating.setText(String.valueOf(movies.getRating()));
+        holder.textViewReleaseDate.setText(movies.getReleasedate());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(holder.itemView.getContext(),DetailMovieActivity.class);
+            intent.putExtra("MOVIES", movies);
+            holder.itemView.getContext().startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayListMovies.size();
+    }
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewTitle, textViewRating, textViewReleaseDate;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.tvTitle);
+            textViewRating = itemView.findViewById(R.id.tvRating);
+            textViewReleaseDate = itemView.findViewById(R.id.tvReleaseDate);
+
+        }
+    }
+}
